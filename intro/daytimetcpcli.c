891b4e41f4546e25c741aee91ead6b6ed8d1642a
icecream <158474361@qq.com>
2019-12-11 02:45:57 -0800

    daytime

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,34 +1,34 @@
-#include	"unp.h"
+#include "unp.h"
 
-int
-main(int argc, char **argv)
-{
-	int					sockfd, n;
-	char				recvline[MAXLINE + 1];
-	struct sockaddr_in	servaddr;
+int main(int argc, char* argv[]){
+    int cli_sockfd;
+    struct sockaddr_in serv_addr;
+    int nr;
+    char line[MAXLINE];
+    if(argc != 2){
+        err_quit("usage: a.out <ipaddress>");
+    }
+    if( (cli_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        err_sys("socket error");
+    }
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(13);
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr.s_addr) <=0){
+        err_quit("inet_pton error for %s", argv[1]);
+    }
 
-	if (argc != 2)
-		err_quit("usage: a.out <IPaddress>");
-
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		err_sys("socket error");
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
-	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-		err_quit("inet_pton error for %s", argv[1]);
-
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
-		err_sys("connect error");
-
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-		recvline[n] = 0;	/* null terminate */
-		if (fputs(recvline, stdout) == EOF)
-			err_sys("fputs error");
-	}
-	if (n < 0)
-		err_sys("read error");
-
-	exit(0);
+    if(connect(cli_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+        err_sys("connect error");
+    }
+    while( (nr = read(cli_sockfd, line, MAXLINE-1)) > 0){
+        line[nr] = '\0';
+        if(fputs(line,stdout) == EOF){
+            err_sys("fputs error");
+        }
+    }
+    if(nr < 0){
+        err_sys("read error");
+    }
+    exit(0);
 }
