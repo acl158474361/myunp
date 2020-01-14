ea1bd0326da28fecab0b75d356a41a45768f009e
icecream <158474361@qq.com>
2020-01-14 05:00:06 -0800

    endian

--- tcpcliserv/endian.c
+++ tcpcliserv/endian.c
@@ -8,6 +8,19 @@ int main(void){
         char c[sizeof(int)];
     } un;
     
+    struct sint{
+        int a;
+        int b;
+    } si;
+
+    si.a = 0x0405;
+    si.b = 0x0f0a;
+
+    char* i = (char*)(&si.a);
+    printf("%d\n", *(i+4));
+    printf("%d\n", *(i+5));
+    printf("%d\n", *(i+6));
+    printf("%d\n", *(i+7));
     un.i = 0x0102;
     printf("%d %d %d %d\n", un.c[0], un.c[1], un.c[2], un.c[3]);
     printf("%d\n", un.i);
