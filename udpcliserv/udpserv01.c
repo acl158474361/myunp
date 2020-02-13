b1737e3ea00e3d523fbf66d2ddef2379be1c735d
icecream <158474361@qq.com>
2020-02-12 23:49:11 -0800

    htons

--- udpcliserv/udpserv01.c
+++ udpcliserv/udpserv01.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv){
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htonl(SERV_PORT);
+    servaddr.sin_port = htons(SERV_PORT);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     Bind(sockfd, (SA*)&servaddr, sizeof(servaddr));
